8ae0b6e643f747549a27592a13deb552b777e491
minamoto-m <masaki.minamoto@prum.jp>
2024-09-24 01:39:55 +0900

    add:404、500ページ作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 Not Found</title>
+</head>
+  <h1>404 Not Found</h1>
+  <p>指定されたページが見つかりません。</p>
+  <a href="{{ route('admin.create') }}">ログインへ</a>
+</body>
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>500 Application Error</title>
+</head>
+  <h1>500 Application Error</h1>
+  <p>エラーが発生しました。時間をおいて再度お試しください。</p>
+</body>
+</html>
